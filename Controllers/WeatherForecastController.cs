855a828dba1d58dd4452032b7b5f4e6c7d1c8e36
MR Bednar <M.R.Bednar@gmail.com>
2024-12-01 02:30:39 -0800

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetAdvent2024.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public void Get()
+        {
+            
+        }
+    }
+}
